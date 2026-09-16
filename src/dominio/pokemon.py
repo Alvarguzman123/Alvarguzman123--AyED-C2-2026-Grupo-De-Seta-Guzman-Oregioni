0036eba5502f2fff1b1927fc76a9d6cc11fed39b
Alvarguzman123 <alvarguzman@gmail.com>
2026-09-16 18:31:50 -0300

    Avance de función recursiva para entrega 2

--- src/dominio/pokemon.py
+++ src/dominio/pokemon.py
@@ -3,6 +3,10 @@ class Pokemon:
         self.numero = numero
         self.nombre = nombre
         self.tipo = tipo
+        #Sirve para establecer la relación de evolución entre los pokemones. Por ejemplo, Bulbasaur evoluciona a Ivysaur, y luego a Venusaur. Por ahora representa un valor nulo, pero luego se puede establecer la relación de evolución entre los pokemones.#
+        self.evolucion = None
 
+    def establecer_evolucion(self, siguiente_epokemon):
+        self.evolucion = siguiente_epokemon
     def __str__(self):
         return f"Nro: {self.numero} | {self.nombre} | Tipo: {self.tipo}"
